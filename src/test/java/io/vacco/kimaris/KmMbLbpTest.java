178f66d44f0999107e4cc1fce83cec43fb2f33e5
jjzazuet <jjzazuet@vacco.io>
2022-05-06 22:18:33 -0400

    - MBLBP image dump support. - Test case updates.

--- src/test/java/io/vacco/kimaris/KmMbLbpTest.java
+++ src/test/java/io/vacco/kimaris/KmMbLbpTest.java
@@ -7,12 +7,12 @@ import j8spec.annotation.DefinedOrder;
 import j8spec.junit.J8SpecRunner;
 import org.junit.runner.RunWith;
 import java.io.File;
+import java.util.Objects;
 import java.util.stream.IntStream;
 
 import static j8spec.J8Spec.*;
 import static io.vacco.sabnock.SkJson.*;
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertFalse;
 
 @DefinedOrder
 @RunWith(J8SpecRunner.class)
@@ -35,15 +35,15 @@ public class KmMbLbpTest {
           true, true, true, true, true, true, true, true
       }));
 
-      short[][] imgIn = new short[][] {
-          { 0,  1,  3,  5,  2,  7, 10,  7, 10,  9},
-          { 3,  7,  6,  9,  3,  8,  1,  8,  5,  8},
-          { 1, 11,  0,  7, 13,  7,  8, 12, 13,  1},
-          {14,  3,  2,  7,  1,  8,  9, 11,  2, 12},
-          { 1,  5, 15,  3,  6,  6, 20, 19, 10,  6},
-          { 8,  1,  2,  6,  7,  3,  2, 11,  0, 15},
-          { 7,  7,  6,  0,  9,  5, 10,  3,  8,  1},
-          {12,  5,  6, 10, 11,  3,  6,  7,  9,  1}
+      short[][] imgIn = new short[][]{
+          {0, 1, 3, 5, 2, 7, 10, 7, 10, 9},
+          {3, 7, 6, 9, 3, 8, 1, 8, 5, 8},
+          {1, 11, 0, 7, 13, 7, 8, 12, 13, 1},
+          {14, 3, 2, 7, 1, 8, 9, 11, 2, 12},
+          {1, 5, 15, 3, 6, 6, 20, 19, 10, 6},
+          {8, 1, 2, 6, 7, 3, 2, 11, 0, 15},
+          {7, 7, 6, 0, 9, 5, 10, 3, 8, 1},
+          {12, 5, 6, 10, 11, 3, 6, 7, 9, 1}
       };
       short[][] imgOut = new short[8][10];
       boolean[] out = new boolean[8];
@@ -69,34 +69,47 @@ public class KmMbLbpTest {
       KmMbLbp.applyToRegion(imgOut, out0, 3, 4, 2, 2, v -> (short) (v / (2 * 2)));
       System.out.println(KmMbLbp.unsignedFrom(out0));
     });
+  }
 
+  private static final String[] imgA = new String[] {
+      "/sample-00.jpg",
+      "/sample-01.jpg"
+  };
+
+  static {
     it("Extracts MBLBP values from an image", () -> {
-      var img = KmMbLbpTest.class.getResource("/sample.jpg");
-      var ip = KmImage.grayPixelsOf(img, null);
-      KmMbLbp.mbLbpImageOf(
-          ip, 1, 1,
-          (dim, data) -> KmImage.writePng(dim.col, dim.row, data, new File("./build/mblbp-out.png"))
-      );
+      for (String path : imgA) {
+        var img = Objects.requireNonNull(KmMbLbpTest.class.getResource(path));
+        var ip = KmImage.grayPixelsOf(img, null);
+        var out = new File(String.format("%s-mblbp.png", img.getFile()).replace(".jpg", ""));
+
+        System.out.println(out.getAbsolutePath());
+
+        KmMbLbp.mbLbpImageOf(ip, 1, 1, (dim, data) -> KmImage.writePng(dim.col, dim.row, data, out));
+      }
     });
     it("Extracts MBLBP histograms from an image", () -> {
-      var img = KmMbLbpTest.class.getResource("/sample.jpg");
-      var ip = KmImage.grayPixelsOf(img, null);
-      var lbpH = KmMbLbp.mbLbpHistogramOf(ip, 1, 1);
-      var chart = obj(
-          kv("tooltip", obj(kv("show", true))),
-          kv("xAxis", obj(
-              kv("type", "category"),
-              kv("data", IntStream.range(0, 256).toArray())
-          )),
-          kv("yAxis", obj(kv("type", "value"))),
-          kv("series", new Object[] {
-              obj(
-                  kv("data", lbpH),
-                  kv("type", "bar")
-              )
-          })
-      );
-      System.out.println(g.toJson(chart));
+      for (String path : imgA) {
+        var img = Objects.requireNonNull(KmMbLbpTest.class.getResource(path));
+        var ip = KmImage.grayPixelsOf(img, null);
+        var lbpH = KmMbLbp.mbLbpHistogramOf(ip, 1, 1);
+        var chart = obj(
+            kv("tooltip", obj(kv("show", true))),
+            kv("xAxis", obj(
+                kv("type", "category"),
+                kv("data", IntStream.range(0, 256).toArray())
+            )),
+            kv("yAxis", obj(kv("type", "value"))),
+            kv("series", new Object[] {
+                obj(
+                    kv("data", lbpH),
+                    kv("type", "bar")
+                )
+            })
+        );
+        System.out.println(g.toJson(chart));
+        System.out.println("============================");
+      }
     });
   }
 }
